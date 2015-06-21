e024893e90097a5568a55d418c9f8a48807b45d7
sanketmaru <sanketmaru@gmail.com>
2015-06-22 00:10:45 +0530

    some changes to reach demo

--- src/helloreact.js
+++ src/helloreact.js
@@ -1,63 +1,87 @@
-
-
-var CommentList = React.createClass({
-
-	render: function(){
-		var commentNodes = this.props.data.map(function (comment) {
-	    return (
-	      <Comment author={comment.author}>
-	        {comment.text}
-	      </Comment>
-	    );
-	  });
-	  return (
-	    <div className="commentList">
-	      {commentNodes}
-	    </div>
-	  );
-	}
-
-});
-
-var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
-});
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        <h1>Comments sanket</h1>
-      </div>
-    );
-  }
-});
-
-var Comment = React.createClass({
-  render: function() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        {this.props.children}
-      </div>
-    );
-  }
-});
-
 var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
+    {
+        name: "Antonin",
+        description: "Web developer"
+    },
+    {
+        name: "Juno",
+        description: "Web designer"
+    },
+    {
+        name: "Alix",
+        description: "Web designer"
+    }
 ];
 
 
-React.render(
-  <CommentBox data={data} />, document.getElementById('content')
-);
+var MetaList = React.createClass({
+    render: function() {
+        return (
+            <ul className="list-meta">
+                <li><a href="#" className="button">Like person</a></li>
+                <li><a href="#" className="button">Block</a></li>
+                <li><a href="#" className="button">Ignore</a></li>
+            </ul>
+        );
+    }
+});
+
+var Person = React.createClass({
+    render: function() {
+        return (
+            <li className="">
+                <h4 className="secondary-dark">
+                    {this.props.person.name}
+                </h4>
+                <p>
+                    {this.props.person.description}
+                </p>
+                <MetaList />
+            </li>
+        );
+    }
+});
+
+var PeopleList = React.createClass({
+    render: function() {
+        //no ng-repeat or foreach
+        var personNodes = this.props.data.map(function(person) {
+            return (
+                <Person person={person} key={person.name} />
+            );
+        });
+
+        return (
+            <ul className="block-list comment-block">
+                {personNodes}
+            </ul>
+        );
+    }
+});
+
+var People = React.createClass({
+    getInitialState: function() {
+        //set initial empty dataset
+        return {data: []};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: '/api/users',
+            dataType: 'json',
+            success: function(data) {
+                //changing state
+                this.setState({data: data})
+            }.bind(this),
+            error: function() {
+
+            }
+        });
+    },
+    render: function() {
+        return (
+            <PeopleList data={this.state.data} />
+        );
+    }
+});
+
+React.render(<People />,document.getElementById('content'));
